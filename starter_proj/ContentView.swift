8c92505a7c2a116d168eeed41ac238d914ee8083
Sabrina Li <sabrinali0312@gmail.com>
2020-03-11 10:14:56 -0400

    Initial Commit

---
+++ starter_proj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  starter_proj
+//
+//  Created by Sabrina Jingyue Li on 3/11/20.
+//  Copyright © 2020 Sabrina Jingyue Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ starter_projTests/starter_projTests.swift
@@ -0,0 +1,34 @@
+//
+//  starter_projTests.swift
+//  starter_projTests
+//
+//  Created by Sabrina Jingyue Li on 3/11/20.
+//  Copyright © 2020 Sabrina Jingyue Li. All rights reserved.
+//
+
+import XCTest
+@testable import starter_proj
+
+class starter_projTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
